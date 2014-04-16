4b54b10d26920ecbe64c9b95ca9fd36f3c32a760
Stefan Hoffmann <stefan991@gmail.com>
2014-04-16 12:48:08 +0200

    first version of build_doc.sh

--- scripts/build_doc.sh
+++ scripts/build_doc.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
+GIT_DIR=$1
+GIT_SHA=$2
+LOG_OUTPUT_FILE=$3
+DOCUMENTATION_OUTPUT_DIR=$4
+
 echo "generating build..."
-sleep 2
+cd $GIT_DIR
+git fetch origin >> $LOG_OUTPUT_FILE 2>&1
+git checkout $GIT_SHA >> $LOG_OUTPUT_FILE 2>&1
+doxygen >> $LOG_OUTPUT_FILE 2>&1
+mv documentation/html $DOCUMENTATION_OUTPUT_DIR
+rm -rf documentation
 echo "done."
 
