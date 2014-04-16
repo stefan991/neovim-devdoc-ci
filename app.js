e164688adca4f03e4eba7c18057e8db93720f05e
Stefan Hoffmann <stefan991@gmail.com>
2014-04-16 17:58:07 +0200

    add express skeleton

---
+++ app.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var path = require('path');
+
+var routes = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.engine('html', require('ejs').__express);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+module.exports = app;
+
+if (!module.parent) {
+    app.listen(5000);
+    console.log('Express started on port 5000');
+}
+
