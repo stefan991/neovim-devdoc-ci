7c39c25c35934980bb16f4a3790135274fe71a51
Stefan Hoffmann <stefan991@gmail.com>
2014-04-18 15:16:39 +0200

    added some logging

--- routes/webhook.js
+++ routes/webhook.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 router.post('/:secret', function(req, res, next) {
     if (req.params.secret !== req.config.secret) {
+        console.log('wrong secret: ' + req.params.secret);
         next(new Error('wrong secret'));
     }
     var event = req.headers['x-github-event'];
@@ -10,16 +11,19 @@ router.post('/:secret', function(req, res, next) {
     if (event === 'push') {
         handle_push(req, res, payload);
     } else {
+        console.log('Ignoring event: ' + event);
         res.send('Ignoring event: ' + event);
     }
 });
 
 function handle_push(req, res, payload) {
     if (payload.ref !== 'refs/heads/' + req.config.branch) {
+        console.log('Ignoring branch: ' + payload.ref);
         res.send('Ignoring branch: ' + payload.ref);
         return;
     }
     if (payload.repository.url !== req.config.repo_url) {
+        console.log('Ignoring repo: ' + payload.repository.url);
         res.send('Ignoring repo: ' + payload.repository.url);
         return;
     }
@@ -28,6 +32,7 @@ function handle_push(req, res, payload) {
     var db = req.db;
     req.db.create_build(head_commit.id, head_commit, function() {
         res.send('OK.');
+        console.log('build created');
         builder.build_next(db, req.config);
     });
 }
